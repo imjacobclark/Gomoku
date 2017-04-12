500134a3a465db7ecd51d0285b128bc2a563f2cf
Jacob Clark <jacob.clark@coop.co.uk>
2017-04-13 00:17:09 +0100

    Change engine uri

--- src/js/GomokuCanvas.js
+++ src/js/GomokuCanvas.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let server = 'http://localhost:8080';
+let server = 'https://gomoku-engine.herokuapp.com';
 let socket = new SockJS(server + '/ws');
 let stompClient = Stomp.over(socket);
 let uuid = null;
